060317d9a3768663d14009c7ab9635b3f73245c9
Jake <jetrussell93@gmail.com>
2016-09-23 14:45:28 -0400

    create test for 404 URL

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,6 +4,15 @@ import app from '../src/app';
 import {expect} from 'chai';
 import request from 'supertest';
 
+describe('GET 404 on no URL', () => {
+  it('should return a 404', (done) => {
+    request(app)
+      .get('/NoUrlHere')
+      .expect(404)
+      .expect('Content-Type', 'text/html; charset=utf-8', done)
+  });
+});
+
 describe('GET /', () => {
   it('should return Hola', (done) => {
     request(app)
