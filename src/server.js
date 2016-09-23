5126322a1f3eab55b8e089f9c33d085851561c99
Jake <jetrussell93@gmail.com>
2016-09-23 14:20:49 -0400

    create basic express server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+process.on('uncaughtException', err => console.error('uncaught exception:', err));
+process.on('unhandledRejection', error => console.log('unhandled rejection:', error));
